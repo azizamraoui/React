5fbdf76ce79c4d5e8143fdd20cb078af0a3f83eb
azizamraoui <amraouiaziz4@gmail.com>
2021-03-09 11:14:49 +0100

    reactproject

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from "./components/NavBar/Header"
+import MyForm from "./components/MyForm"
+import React from 'react'
+import "./components/NavBar/NavBar.css"
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <MyForm/>
+    </div>
+  );
+}
+
+export default App;
